254a89565e5030f2355cf9cd866b2ac2a89f08eb
isheeze <isheeze@gmail.com>
2024-02-21 23:09:04 -0800

    revalidate issue

--- sanity/lib/sanityFetch.ts
+++ sanity/lib/sanityFetch.ts
@@ -36,7 +36,7 @@ export async function sanityFetch<QueryResponse>({
             //     perspective: "previewDrafts",
             // }),
             
-            next:{revalidate:1}
+            next:{revalidate:10}
               
             // next: {
             //     tags,
